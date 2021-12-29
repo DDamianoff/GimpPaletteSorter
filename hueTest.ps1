c9e39416828dd960c0af71279d79da296405e81a
TeiV2 <75574873+TeiV2@users.noreply.github.com>
2021-12-29 16:48:52 -0300

    Spaces and indents

--- hueTest.ps1
+++ hueTest.ps1
@@ -1,13 +1,15 @@
 #!/usr/bin/pwsh
 
 function FunctionName {
-    param(
+    param (
         [Parameter(ParameterSetName = "Red", position = 0)]
         [Parameter(ParameterSetName = "Green", position = 1)]
         [Parameter(ParameterSetName = "Blue", position = 2)]
 
         [int]$Red,
         [int]$Green,
-        [int]$Blue)
+        [int]$Blue
+    )
+    
     
 }
