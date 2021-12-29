95b6206d8de710d7c7c33f7310158c3fed3af4e1
TeiV2 <75574873+TeiV2@users.noreply.github.com>
2021-12-29 16:46:34 -0300

    Basic fuction structure

--- hueTest.ps1
+++ hueTest.ps1
@@ -1,1 +1,13 @@
 #!/usr/bin/pwsh
+
+function FunctionName {
+    param(
+        [Parameter(ParameterSetName = "Red", position = 0)]
+        [Parameter(ParameterSetName = "Green", position = 1)]
+        [Parameter(ParameterSetName = "Blue", position = 2)]
+
+        [int]$Red,
+        [int]$Green,
+        [int]$Blue)
+    
+}
