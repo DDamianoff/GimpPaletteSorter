574e262db70e055787a06514010ac1a04f604537
TeiV2 <75574873+TeiV2@users.noreply.github.com>
2021-12-29 16:49:07 -0300

    Spaces and indents

--- hueTest.ps1
+++ hueTest.ps1
@@ -2,9 +2,9 @@
 
 function FunctionName {
     param (
-        [Parameter(ParameterSetName = "Red", position = 0)]
-        [Parameter(ParameterSetName = "Green", position = 1)]
-        [Parameter(ParameterSetName = "Blue", position = 2)]
+        [Parameter(ParameterSetName = "Red",    position = 0)]
+        [Parameter(ParameterSetName = "Green",  position = 1)]
+        [Parameter(ParameterSetName = "Blue",   position = 2)]
 
         [int]$Red,
         [int]$Green,
