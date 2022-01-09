97eb2e25e92dce1ffde032ef586d548440bb0f61
TeiV2 <75574873+TeiV2@users.noreply.github.com>
2022-01-09 16:27:38 -0300

    integrated parser functions into ColorClass and constructor

--- paletteParser.ps1
+++ paletteParser.ps1
@@ -14,6 +14,125 @@ class Color {
     [string]$HEX;
     [RGB]$RGB;
     [HSL]$HSL;
+
+    Color([string]$Hex) {
+        $this.HEX = $Hex;
+
+        $this.RGB = $this.ConvertToRgb($Hex);
+        $this.HSL = $this.ConvertToHsl($this.RGB);
+    }
+
+    [RGB]ConvertToRgb([string]$HexValue) {
+        if ($HexValue[0] -eq "#") 
+        {
+            $HexValue = $HexValue.Substring(1,6);
+        }
+        
+        [int]$this.Red    = ([int]($hexValue.Substring(0,2)));
+        [int]$this.Green  = ([int]($hexValue.Substring(2,2)));
+        [int]$this.Blue   = ([int]($hexValue.Substring(4,2)));
+
+        <#
+            TODO: change this by "new-objet" syntax.
+        #>
+        return [RGB]::new($this.Red,$this.Green,$this.Blue);
+    }
+
+    [HSL]ConvertToHsl([RGB]$RgbValue) {
+        
+        [double]$Red    = $RgbValue.Red;
+        [double]$Green  = $RgbValue.Green;
+        [double]$Blue   = $RgbValue.Blue;
+
+        $this.Hue;
+        $this.Saturation;
+        $this.Lightness;
+        $this.Chroma;
+
+        <# ensure values are int #>
+        Set-Variable    -Name   Red             `
+                        -Value  ([int]$Red)     ;
+
+        Set-Variable    -Name   Green           `
+                        -Value  ([int]$Green)   ;
+
+        Set-Variable    -Name   Blue            `
+                        -Value  ([int]$Blue)    ;
+
+
+        Set-Variable    -Name   Red             `
+                        -Value  ($Red   / 256)  ;
+        
+        Set-Variable    -Name   Green           `
+                        -Value  ($Green / 256)  ;
+        
+        Set-Variable    -Name   Blue            `
+                        -Value  ($Blue  / 256)  ;
+
+
+        [double]$Maximum = (@($Red,$Green,$Blue) | Measure-Object -Maximum).Maximum;
+        [double]$Minimum = (@($Red,$Green,$Blue) | Measure-Object -Minimum).Minimum;
+        
+        Set-Variable    -Name   Chroma                  `
+                        -Value  ($Maximum - $Minimum)   ;
+
+
+        if ($this.Chroma -eq 0) {
+            Set-Variable    -Name   Hue `
+                            -Value  0   ;
+        }
+        else {
+            switch ($Maximum) {
+                $Red        {
+                    Set-Variable                                                    `
+                        -Name   Hue                                                 `
+                        -Value  ((60 * (($Green - $Blue) / $this.Chroma)  + 360) % 360)  ;
+                }
+
+                $Green      {
+                    Set-Variable                                                    `
+                        -Name   Hue                                                 `
+                        -Value  (60 * (($Blue - $Red)   / $this.Chroma)  + 120)          ;
+                }
+
+                $Blue       {
+                    Set-Variable                                                    `
+                        -Name   Hue                                                 `
+                        -Value  (60 * (($Red - $Green)  / $this.Chroma)  + 240)          ; 
+                }
+
+                $Minimum    {
+                    Set-Variable    -Name   Hue `
+                                    -Value  0   ;
+                }
+            }
+
+            Remove-Variable -Name "Switch";
+        }
+
+
+        Set-Variable                                        `
+            -Name   Lightness                               `
+            -Value  ([double](($Maximum + $Maximum) / 2))   ;
+
+
+
+        if ($Maximum -eq $Minimum) {
+            Set-Variable            `
+                -Name   Saturation  `
+                -Value  0           ;
+        }
+        elseif ($this.Lightness -le 0.5) {
+            Set-Variable    -Name   Saturation                                              `
+                            -Value  ([double]($Maximum - $Minimum) / (2 * $this.Lightness))      ;
+        }
+        elseif ($this.Lightness -gt 0.5) {
+            Set-Variable    -Name   Saturation                                              `
+                            -Value  ([double]($Maximum - $Minimum) / (2 - 2 * $this.Lightness))  ;
+        }
+
+        return [HSL]::new($this.Hue,$this.Saturation,$this.Lightness);
+    }
 }
 
 
@@ -50,109 +169,7 @@ class HSL {
 #>
 
 
-function ConvertTo-Hsl {
-    param (
-        [Parameter()]
-        [ValidateRange(0,255)]
-        [double]$Red,
 
-        [Parameter()]
-        [ValidateRange(0,255)]
-        [double]$Green,
-
-        [Parameter()]
-        [ValidateRange(0,255)]
-        [double]$Blue)
-    
-    New-Variable -Name Hue;
-    New-Variable -Name Saturation;
-    New-Variable -Name Lightness;
-    New-Variable -Name Chroma;
-
-    <# ensure values are int #>
-    Set-Variable    -Name   Red             `
-                    -Value  ([int]$Red)     ;
-
-    Set-Variable    -Name   Green           `
-                    -Value  ([int]$Green)   ;
-
-    Set-Variable    -Name   Blue            `
-                    -Value  ([int]$Blue)    ;
-
-
-    Set-Variable    -Name   Red             `
-                    -Value  ($Red   / 256)  ;
-    
-    Set-Variable    -Name   Green           `
-                    -Value  ($Green / 256)  ;
-    
-    Set-Variable    -Name   Blue            `
-                    -Value  ($Blue  / 256)  ;
-
-
-    [double]$Maximum = (@($Red,$Green,$Blue) | Measure-Object -Maximum).Maximum;
-    [double]$Minimum = (@($Red,$Green,$Blue) | Measure-Object -Minimum).Minimum;
-    
-    Set-Variable    -Name   Chroma                  `
-                    -Value  ($Maximum - $Minimum)   ;
-
-
-    if ($Chroma -eq 0) {
-        Set-Variable    -Name   Hue `
-                        -Value  0   ;
-    }
-    else {
-        switch ($Maximum) {
-            $Red        {
-                Set-Variable                                                    `
-                    -Name   Hue                                                 `
-                    -Value  ((60 * (($Green - $Blue) / $Chroma)  + 360) % 360)  ;
-            }
-
-            $Green      {
-                Set-Variable                                                    `
-                    -Name   Hue                                                 `
-                    -Value  (60 * (($Blue - $Red)   / $Chroma)  + 120)          ;
-            }
-
-            $Blue       {
-                Set-Variable                                                    `
-                    -Name   Hue                                                 `
-                    -Value  (60 * (($Red - $Green)  / $Chroma)  + 240)          ; 
-            }
-
-            $Minimum    {
-                Set-Variable    -Name   Hue `
-                                -Value  0   ;
-            }
-        }
-
-        Remove-Variable -Name "Switch";
-    }
-
-
-    Set-Variable                                        `
-        -Name   Lightness                               `
-        -Value  ([double](($Maximum + $Maximum) / 2))   ;
-
-
-
-    if ($Maximum -eq $Minimum) {
-        Set-Variable            `
-            -Name   Saturation  `
-            -Value  0           ;
-    }
-    elseif ($Lightness -le 0.5) {
-        Set-Variable    -Name   Saturation                                              `
-                        -Value  ([double]($Maximum - $Minimum) / (2 * $Lightness))      ;
-    }
-    elseif ($Lightness -gt 0.5) {
-        Set-Variable    -Name   Saturation                                              `
-                        -Value  ([double]($Maximum - $Minimum) / (2 - 2 * $Lightness))  ;
-    }
-
-    return [HSL]::new($Hue,$Saturation,$Lightness);
-}
 
 
 New-Variable    -Name           header                                              `
